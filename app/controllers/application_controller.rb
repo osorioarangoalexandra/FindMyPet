6fb615f9ce03160f6ed03d4478a95ad057a77a3e
“Alexandra <osorioarangoalexandra@gmail.com>
2020-02-10 18:44:12 -0500

    Db+Device

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+
+    before_action :authenticate_user!
+    protect_from_forgery with: :exception
+    
+        # Prevent CSRF attacks by raising an exception.
+        # For APIs, you may want to use :null_session instead.
+     
 end
+
--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -4,4 +4,52 @@ def index
     @owners = Owner.all
 end
 
+
+def show
+    @owner = Owner.find(params[:id])
+end
+
+def new
+    @owner = Owner.new
+end
+
+def create
+    @owner = owner.new(owner_params)
+
+    if @owner.save
+        redirect_to owners_path, notice: 'Your Profile was succesfully created.'
+    else
+        render :new
+    end
+end
+
+def edit
+    @owner = Owner.find(params[:id])
+end
+
+def update
+    owner = Owner.find(params["id"])
+    owner.update(owner_params)
+    redirect_to owners_path
+end
+
+def destroy
+    
+
+    Owner.find(params["id"]).destroy
+    redirect_to owners_path
+    
+end
+# def create
+#     # burger es variable de la clase hamburguesa
+#     burger = Burger.create(burger_params)
+#     redirect_to burger
+# end
+
+private
+
+def owner_params
+    params.require(:owner).permit(:name, :lastname, :phone, :email, :image)
+end
+
 end
